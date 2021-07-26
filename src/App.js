fc34d50ec4bb2eff4f2f25b34f298eb086a7c3ce
Gino Swanepoel <ginoleemusic@live.com>
2021-07-27 00:39:39 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Balance from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList'
@@ -12,13 +13,18 @@ import './App.css';
 function App() {
   return (
     <GlobalProvider>
+
       <Header />
+
       <div className="container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
         <AddTransaction />
       </div>
+      
+      <Footer />
+
     </GlobalProvider>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            Expenses Tracker App made by Gino Swanepoel <br/>
+            © All rights reserved.
+        </div>
+    )
+}
